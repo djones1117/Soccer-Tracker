2fd3590ef424ca3096a1f7754c6ae983eb1882e5
djones1117 <socjones21@yahoo.com>
2023-07-19 09:17:14 -0400

    basic styling

--- routes/teams.js
+++ routes/teams.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const teamsCtrl = require("../controllers/teams");
-
+//shows the home page 
 router.get("/", teamsCtrl.index);
-
+// gets the new page to update players
 router.get("/new", teamsCtrl.new);
-
+// shows the show teams details
 router.get("/:id", teamsCtrl.show);
-
+//creates team
 router.post("/", teamsCtrl.create);
 
 module.exports = router;
