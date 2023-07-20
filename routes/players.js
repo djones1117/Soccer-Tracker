1c0589668bfd4b2aff8edbeeb488b6fa71048be7
djones1117 <socjones21@yahoo.com>
2023-07-20 09:52:19 -0400

    notes

--- routes/players.js
+++ routes/players.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const playersCtrl = require("../controllers/players");
-
+//creates player
 router.post("/teams/:id/players", playersCtrl.create);
-
+//puts update on server
 router.put("/players/:id", playersCtrl.update);
-
+//grab players id to edit
 router.get("/players/:id/edit", playersCtrl.edit);
-
+//deletes player
 router.delete("/players/:id", playersCtrl.delete);
 
 module.exports = router;
--- routes/teams.js
+++ routes/teams.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const teamsCtrl = require("../controllers/teams");
 //shows the home page 
 router.get("/", teamsCtrl.index);
-// gets the new page to update players
+// gets the new page to create a new team
 router.get("/new", teamsCtrl.new);
 // shows the show teams details
 router.get("/:id", teamsCtrl.show);
-//creates team
+//posts team
 router.post("/", teamsCtrl.create);
 
 module.exports = router;
